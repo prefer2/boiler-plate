c55230cc7b3bd4a90d32f7b4473eddaf758294ca
username <sunho6200@gmail.com>
2021-03-01 15:01:25 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
 
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
+const config = require('./config/key');
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sunho620:tjsgh620@boilerplate.kixwg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected...'))
   .catch(err=>console.log(err))
+
 app.get('/', (req,res)=>res.send('Hello World!'))
 
 app.listen(port, ()=>console.log(`Example app listening on port ${port}!`))
 
+app.post('/register', (req,res)=>{
+    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    })
+})
